aafdf916eb3ceee9dfae1e46bef6a93080041943
Gurmandeep18 <gurmandeep23@yahoo.com>
2017-02-19 18:07:15 +0530

    Changed to PDO Queries

--- DatabaseConnection/Login.php
+++ DatabaseConnection/Login.php
@@ -7,11 +7,15 @@
  * 
  */
 
+
+
 require_once('./Connection.php');
 $connection = new Connection();
 $databaseConnection = $connection -> dbConnect();
 
-session_start();
+
+include_once 'User.php';
+$user = new USER($databaseConnection);
 
 // username and password sent from form
 $username=$_POST['username'];
@@ -23,42 +27,23 @@ $password = stripslashes($password);
 $username = mysql_real_escape_string($username);
 $password = mysql_real_escape_string($password);
 
-$sql="SELECT * FROM naukrisamachaar_user WHERE user_emain='$username' and user_password='$password'";
-$result=mysql_query($sql);
+$status = $user ->login($username, $password);
 
-$count=mysql_num_rows($result);
-
-if($count==1 && $result){ 
-    while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
-        $_SESSION["username"] = $row[1];
-        $_SESSION["userId"] =  $row[0];
-    }
-    
-    $userId = $_SESSION["userId"];
-    
-    //To get the role
-    $sql = "SELECT * FROM naukrisamachaar_userroles WHERE user_id = '$userId'";
-    $result=mysql_query($sql);
-    $count=mysql_num_rows($result);
-    
-    if($count == 1 && $result) {
-        while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
-            $_SESSION["role"] = $row[1];
-        }
-    }
-    
-    if($_SESSION["role"] == "ADMIN") {
-       header("location:../Dashboard.php"); 
-    } else if($_SESSION["role"] == "USER") {
-       echo "User Account Page not yet linked"; 
+if($status != 0){
+    $role = $user ->role($status);
+    if($role == "ADMIN") {
+        $user->redirect("./dashboard.php");
+    } 
+    else if($role == "USER"){
+        //$user->redirect("USERPROFILEPAGE") 
     } else {
-       echo "Undefined Role";
+        
     }
+} else {
+    echo 'No Such User Exist';
 }
-else {
-    $_SESSION["status"] = "Wrong Credential";
-    header("location:Test.php"); 
-    echo "Wrong Username or Password";
-}
+
+
+
 
 
